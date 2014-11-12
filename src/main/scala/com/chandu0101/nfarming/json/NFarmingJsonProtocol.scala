b7b2f1727d149f33be098db1ae11afd5c2df76a7
chandu0101 <chandu.csu2010@gmail.com>
2014-11-13 05:17:29 +0530

    added location model

--- src/main/scala/com/chandu0101/nfarming/json/NFarmingJsonProtocol.scala
+++ src/main/scala/com/chandu0101/nfarming/json/NFarmingJsonProtocol.scala
@@ -1,7 +1,7 @@
 package com.chandu0101.nfarming.json
 
 import com.chandu0101.nfarming.error.Error
-import com.chandu0101.nfarming.model.Seed
+import com.chandu0101.nfarming.model.{Position, Seed}
 import spray.json.DefaultJsonProtocol
 
 /**
@@ -9,6 +9,7 @@ import spray.json.DefaultJsonProtocol
  */
 object NFarmingJsonProtocol extends DefaultJsonProtocol{
 
+  implicit val positionJson = jsonFormat2(Position.apply)
   implicit  val seedJson = jsonFormat5(Seed.apply)
   implicit  val errorJson = jsonFormat3(Error)
 }
--- src/main/scala/com/chandu0101/nfarming/model/Seed.scala
+++ src/main/scala/com/chandu0101/nfarming/model/Seed.scala
@@ -5,7 +5,32 @@ import reactivemongo.bson.{BSONDocument, BSONDocumentReader, BSONDocumentWriter}
 /**
  * Created by chandrasekharkode on 11/10/14.
  */
-case class Seed(name: String, category: String, duration: String, location: String, info: String)
+case class Seed(name: String, category: String, duration: String, location: Position, info: String)
+
+case class Position(long : Int, lat: Int)
+
+object Position {
+
+  val LONGITUDE = "lon"
+  val LATITUDE = "lan"
+
+  implicit object PositionReader extends BSONDocumentReader[Position] {
+    override def read(bson: BSONDocument): Position = {
+      val lon = bson.getAs[Int](LONGITUDE).get
+      val lat = bson.getAs[Int](LATITUDE).get
+      Position(lon, lat)
+    }
+  }
+
+  implicit object PositionWriter extends BSONDocumentWriter[Position] {
+    override def write(t: Position): BSONDocument = {
+      BSONDocument(LONGITUDE -> t.long,
+        LATITUDE -> t.lat)
+
+    }
+  }
+
+}
 
 object Seed {
   val ID = "_id"
@@ -14,13 +39,14 @@ object Seed {
   val DURATION = "duration"
   val LOCATION = "location"
   val INFO = "info"
+
   // needed for reactive mongo
   implicit object SeedReader extends BSONDocumentReader[Seed] {
     def read(doc: BSONDocument): Seed = {
       val name = doc.getAs[String](NAME).get
       val category = doc.getAs[String](CATEGORY).get
       val duration = doc.getAs[String](DURATION).get
-      val location = doc.getAs[String](LOCATION).get
+      val location = doc.getAs[Position](LOCATION).get
       val info = doc.getAs[String](INFO).get
 
       Seed(name, category, duration, location, info);
