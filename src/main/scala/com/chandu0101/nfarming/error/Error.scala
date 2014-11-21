6ff3a87899be2aa56a8b86596c9d1d3dfbfc3f70
chandu0101 <chandu.csu2010@gmail.com>
2014-11-21 12:45:08 +0530

    modified document structure

--- src/main/scala/com/chandu0101/nfarming/error/Error.scala
+++ src/main/scala/com/chandu0101/nfarming/error/Error.scala
@@ -3,4 +3,4 @@ package com.chandu0101.nfarming.error
 /**
  * Created by chandrasekharkode on 11/12/14.
  */
-case class Error(message:String , errorType : String,contact:String="nfarming@gmail.com")
+case class Error(message:String , errorType : String,contact:String="nfarming.revolution@gmail.com")
